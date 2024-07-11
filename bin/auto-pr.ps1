b534fd2d7b8b30eb81adf23a7fa1180b6f0fabf7
inertia <inertialiu@gmail.com>
2024-07-11 15:08:21 +0800

    update README

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "inertia42/Chamber:master"
+    [String]$upstream = "inertia42/chamber:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
